6d064531fad078f9e98d741f5354f561540243f2
chkp-stuartgreen <stuartg@checkpoint.com>
2021-07-13 12:54:39 +0100

    moved gateway property to vpn domain

--- 73_s2s_vpn_setup.py
+++ 73_s2s_vpn_setup.py
@@ -50,8 +50,8 @@ vpn_props = {}
 vpn_props['name'] = "S2S_test"
 vpn_props['encryption-method'] = "ikev2 only"
 vpn_props['encryption-suite'] = "suite-b-gcm-256"
-vpn_props['gateways'] = "dummy_gw"
 vpn_props['override-vpn-domains'] = {}
+vpn_props['override-vpn-domains']['gateway'] = "dummy_gw"
 vpn_props['override-vpn-domains']['vpn-domain'] = "S2S_empty_group"
 resp = apiCall.send_command('add-vpn-community-meshed', data = vpn_props)
 print(resp)
